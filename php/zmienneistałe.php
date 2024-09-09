8eb837d2aea535aaab13910d999f1026d71d83c0
protekti <78166534+protekti@users.noreply.github.com>
2024-09-09 08:32:58 +0200

    090.09.2024 Zmienne I Stałe

---
+++ php/zmienneistałe.php
@@ -0,0 +1,20 @@
+<?php
+    $a=100;
+    var_dump($a);
+    echo "<br>";
+    print_r($a);
+    echo "<br> a=$a"."<br>";
+    $b="Ala ma kota";
+    echo $b."<br>";
+    var_dump($b);
+    echo "<br>";
+    $c=false;
+    var_dump($c);
+    define("zzz",300);
+    echo "<br>".(zzz*2)."<br>";
+    echo "<strong>Wartość</strong>
+    <section>a=$a<br></section>";
+    echo "Wartość a=$a<br>";
+    echo "Wartość a=$a<br>";
+    print($a);
+?>
